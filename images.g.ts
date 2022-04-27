720b5cf0f7d275a6ddd8e556074e3f92b047c61e
DABLONKIE <94186808+DABLONKIE@users.noreply.github.com>
2022-04-27 17:12:14 +0200

    Added back sprites.

---
+++ images.g.ts
@@ -0,0 +1,219 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "whitePawn":return img`
+. . . . f f f f . . . . 
+. . . f 1 1 1 1 f . . . 
+. . . f 1 1 1 1 f . . . 
+. . . f 1 1 1 1 f . . . 
+. . . . f 1 1 f . . . . 
+. . . f 4 4 4 4 f . . . 
+. . . . f 1 1 f . . . . 
+. . . f 1 1 1 1 f . . . 
+. . . f 1 1 1 1 f . . . 
+. . f 1 1 1 4 4 4 f . . 
+. . f 4 4 4 4 4 4 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image2":
+            case "whiteBishop":return img`
+. . . . . f f . . . . . 
+. . . . f 1 4 f . . . . 
+. . . . . f f . . . . . 
+. . . . f 1 1 f . . . . 
+. . . f 1 4 1 1 f . . . 
+. . . f 1 1 5 1 f . . . 
+. . . . f 1 1 f . . . . 
+. . . f f f f f f . . . 
+. . f 5 5 5 5 5 5 f . . 
+. . . f 1 1 4 4 f . . . 
+. . f 4 4 4 4 4 4 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image3":
+            case "whiteRook":return img`
+. . f f f f f f f f . . 
+. . f 4 f 4 4 f 4 f . . 
+. . f 1 1 1 1 1 1 f . . 
+. . f f 5 5 5 5 f f . . 
+. . . f 1 5 1 5 f . . . 
+. . f f 5 5 5 5 f f . . 
+. . f 1 5 1 5 1 5 f . . 
+. . f 5 5 5 5 5 5 f . . 
+. f f 5 1 5 1 5 4 f f . 
+. f 1 1 1 1 4 4 4 4 f . 
+. f 4 4 4 4 4 4 4 4 f . 
+. f f f f f f f f f f . 
+`;
+            case "image4":
+            case "whiteKnight":return img`
+. . . . f . . . . . . . 
+. . . f 5 f f f . . . . 
+. . f 1 1 1 1 5 f . . . 
+. f 1 4 1 1 1 1 5 f . . 
+. f 1 1 1 1 1 1 1 5 f . 
+. f 1 1 f 1 1 1 1 1 f . 
+. f f f . f 1 1 1 1 f . 
+. . . f f 1 1 1 1 f . . 
+. . f 1 1 1 1 1 1 f . . 
+. . f f 5 5 5 5 f f . . 
+. . f 4 4 4 4 4 4 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image5":
+            case "whiteQueen":return img`
+. . . . . f f . . . . . 
+. . . . f 4 4 f . . . . 
+. . . f 4 f f 4 f . . . 
+. . f 5 f 5 5 f 5 f . . 
+. . . f 1 1 1 1 f . . . 
+. . . f 1 1 1 1 f . . . 
+. . . . f 1 1 f . . . . 
+. . . . f 1 1 f . . . . 
+. . . . f 1 1 f . . . . 
+. . . f 1 1 1 1 f . . . 
+. . f 4 4 4 4 4 4 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image6":
+            case "whiteKing":return img`
+. . . . . f f . . . . . 
+. . . . f 5 5 f . . . . 
+. . . f 5 5 5 5 f . . . 
+. . . . f 5 5 f . . . . 
+. . f f f f f f f f . . 
+. . f 5 5 5 5 5 5 f . . 
+. . . f 1 1 1 1 f . . . 
+. . . f 1 1 1 1 f . . . 
+. . . . f 1 1 f . . . . 
+. . . f 1 1 1 1 f . . . 
+. . . f 1 1 4 4 f . . . 
+. . f 4 4 4 4 4 4 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image7":
+            case "blackPawn":return img`
+. . . . f f f f . . . . 
+. . . f 7 7 7 7 f . . . 
+. . . f 7 7 7 7 f . . . 
+. . . f 7 7 7 7 f . . . 
+. . . . f 7 7 f . . . . 
+. . . f 8 8 8 8 f . . . 
+. . . . f 7 7 f . . . . 
+. . . f 7 7 7 7 f . . . 
+. . . f 7 7 7 7 f . . . 
+. . f 7 7 7 6 6 6 f . . 
+. . f 6 6 6 6 6 6 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image8":
+            case "blackBishop":return img`
+. . . . . f f . . . . . 
+. . . . f 7 6 f . . . . 
+. . . . . f f . . . . . 
+. . . . f 7 7 f . . . . 
+. . . f 7 6 7 7 f . . . 
+. . . f 7 7 8 7 f . . . 
+. . . . f 7 7 f . . . . 
+. . . f f f f f f . . . 
+. . f 8 8 8 8 8 8 f . . 
+. . . f 7 7 6 6 f . . . 
+. . f 6 6 6 6 6 6 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image9":
+            case "blackRook":return img`
+. . f f f f f f f f . . 
+. . f 6 f 6 6 f 6 f . . 
+. . f 7 7 7 7 7 7 f . . 
+. . f f 8 8 8 8 f f . . 
+. . . f 7 8 7 8 f . . . 
+. . f f 8 8 8 8 f f . . 
+. . f 7 8 7 8 7 8 f . . 
+. . f 8 8 8 8 8 8 f . . 
+. f f 8 7 8 7 8 6 f f . 
+. f 7 7 7 7 6 6 6 6 f . 
+. f 6 6 6 6 6 6 6 6 f . 
+. f f f f f f f f f f . 
+`;
+            case "image10":
+            case "blackKnight":return img`
+. . . . f . . . . . . . 
+. . . f 8 f f f . . . . 
+. . f 7 7 7 7 8 f . . . 
+. f 7 6 7 7 7 7 8 f . . 
+. f 7 7 7 7 7 7 7 8 f . 
+. f 7 7 f 7 7 7 7 7 f . 
+. f f f . f 7 7 7 7 f . 
+. . . f f 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 f . . 
+. . f f 8 8 8 8 f f . . 
+. . f 6 6 6 6 6 6 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image11":
+            case "blackQueen":return img`
+. . . . . f f . . . . . 
+. . . . f 6 6 f . . . . 
+. . . f 6 f f 6 f . . . 
+. . f 8 f 8 8 f 8 f . . 
+. . . f 7 7 7 7 f . . . 
+. . . f 7 7 7 7 f . . . 
+. . . . f 7 7 f . . . . 
+. . . . f 7 7 f . . . . 
+. . . . f 7 7 f . . . . 
+. . . f 7 7 7 7 f . . . 
+. . f 6 6 6 6 6 6 f . . 
+. . f f f f f f f f . . 
+`;
+            case "image12":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image13":
+            case "blackKing":return img`
+. . . . . f f . . . . . 
+. . . . f 8 8 f . . . . 
+. . . f 8 8 8 8 f . . . 
+. . . . f 8 8 f . . . . 
+. . f f f f f f f f . . 
+. . f 8 8 8 8 8 8 f . . 
+. . . f 7 7 7 7 f . . . 
+. . . f 7 7 7 7 f . . . 
+. . . . f 7 7 f . . . . 
+. . . f 7 7 7 7 f . . . 
+. . . f 7 7 6 6 f . . . 
+. . f 6 6 6 6 6 6 f . . 
+. . f f f f f f f f . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
